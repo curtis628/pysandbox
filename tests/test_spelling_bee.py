e592cdfd5cc6ed6bf607e8dfe3ea61e564f2cfee
Tyler Curtis <tcurtis@vmware.com>
2021-12-29 14:02:40 -0800

    Increase code coverage

--- tests/test_spelling_bee.py
+++ tests/test_spelling_bee.py
@@ -1,10 +1,11 @@
 import shutil
-import typing
+import sys
 from pathlib import Path
+from typing import Callable, Generator, Optional, Type
 
 import pytest
 
-from pysandbox.spelling_bee import solve_puzzle
+from pysandbox.spelling_bee import main, solve_puzzle
 
 TEST_DICTIONARY: list[str] = [
     "apple",
@@ -17,7 +18,7 @@ TEST_DICTIONARY: list[str] = [
 
 
 @pytest.fixture(scope="session")
-def test_dictionary_path(tmp_path_factory: pytest.TempPathFactory) -> typing.Generator[Path, None, None]:
+def test_dictionary_path(tmp_path_factory: pytest.TempPathFactory) -> Generator[Path, None, None]:
     """Creates a small, temporary dictionary text file based on `TEST_DICTIONARY`"""
     dictionary_with_newlines = [line + "\n" for line in TEST_DICTIONARY]
     dictionary_path = tmp_path_factory.getbasetemp() / "test-dictionary.txt"
@@ -69,3 +70,44 @@ def test_bad_dictionary(tmp_path: Path) -> None:
     with pytest.raises(RuntimeError) as excinfo:
         solve_puzzle("a", "plebn", dictionary_file=not_exists)
     assert "does not exist" in str(excinfo.value)
+
+
+def _run_with_argv(method: Callable[[], None], new_argv: list[str] = []) -> None:
+    """Helper method that updates sys.argv (and puts it back after)"""
+    old_argv = sys.argv
+    try:
+        sys.argv = new_argv
+        method()
+    finally:
+        sys.argv = old_argv
+
+
+def _run_and_expect(
+    method: Callable[[], None],
+    test_argv: list[str] = [],
+    raises: Optional[Type[SystemExit]] = None,
+    check_code: bool = False,
+) -> None:
+    """Helper that runs `method` with `test_argv`. Expects `error_class` with optional `exit_code`"""
+    if raises:
+        with pytest.raises(raises) as excinfo:
+            _run_with_argv(method, test_argv)
+        if check_code:
+            assert excinfo.value.code == 0
+    else:
+        _run_with_argv(method, test_argv)
+
+
+def test_parse_help() -> None:
+    test_argv = ["pysandbox/spelling_bee.py", "--help"]
+    _run_and_expect(lambda: main(), test_argv, raises=SystemExit, check_code=True)
+
+
+def test_e2e(test_dictionary_path: Path) -> None:
+    test_argv = [
+        "pysandbox/spelling_bee.py",
+        "--must-letter=a",
+        "--optional-letters=plebn",
+        f"--dictionary-file={test_dictionary_path}",
+    ]
+    _run_and_expect(lambda: main(), test_argv)
